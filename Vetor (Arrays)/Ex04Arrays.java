818d889bf6c6a4cacc4aed909e4b22aab53cb849
SergioRibeiro7820 <sergioribeiro782015@gmail.com>
2023-03-25 15:47:39 -0300

    Ex array

---
+++ Vetor (Arrays)/Ex04Arrays.java
@@ -0,0 +1,12 @@
+public class Ex04Arrays {
+    public static void main(String[] args){
+        //4-) Criar um vetor de tamanho 3
+
+        int[] idades;
+        idades = new int[3];
+        idades[0] = 31;
+        idades[1] = 30;
+        idades[2] = 29;
+        System.out.println(idades[2]); //println imprimir linha
+    }
+}
---
+++ Vetor (Arrays)/Ex07Arrays.java
@@ -0,0 +1,25 @@
+public class Ex07Arrays {
+    public static void main(String[] args){
+        //7-) Faça um algoritimo para ler todos os meses do ano, armazene em um vetor.
+
+        String[] ano2023;
+        
+        ano2023 = new String[12];
+        ano2023[0] = "Janeiro";
+        ano2023[1] = "Fevereiro";
+        ano2023[2] = "Março";
+        ano2023[3] = "Abril";
+        ano2023[4] = "Maio";
+        ano2023[5] = "Junho";
+        ano2023[6] = "Julho";
+        ano2023[7] = "Agosto";
+        ano2023[8] = "Setembro";
+        ano2023[9] = "Outubro";
+        ano2023[10] = "Novembro";
+        ano2023[11] = "Dezembro";
+
+            //System.out.println(ano2023[3]); //println imprimir linha
+
+            System.out.println(ano2023.length); //length = saber o tamanho do vetor.
+        }
+    }
